88653639f6d13514ffd1d98b22613ff907963127
Jocely Marques <jocelymarques1@gmail.com>
2025-10-07 18:15:30 -0300

    horfix: ajust deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/shopNet/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  publicDir: 'public'
+  base: '/shopNet/'
 })
