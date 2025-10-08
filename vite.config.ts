828f1b0cfb50443e4051b0e994c456fa85a07d88
Jocely Marques <jocelymarques1@gmail.com>
2025-10-08 08:07:54 -0300

    hotfix: ajust deploy route

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/shopnet/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+  base: '/shopnet'
 })
